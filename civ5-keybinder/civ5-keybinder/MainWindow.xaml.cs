adaf645bfe920f9306941f3d816f171511cb0285
EDWARD-DESKTOP\edwar <edwardnoe@icloud.com>
2019-04-02 21:37:22 -0400

    Added list of hotkeys

--- civ5-keybinder/civ5-keybinder/MainWindow.xaml.cs
+++ civ5-keybinder/civ5-keybinder/MainWindow.xaml.cs
@@ -24,12 +24,7 @@ namespace civ5_keybinder
         {
             InitializeComponent();
 
-            List<Hotkey> hotkeys = new List<Hotkey>();
-            hotkeys.Add(new Hotkey(1, "Civilopedia"));
-            hotkeys[0].DefineBinding("F1", false, false, false);
-
-            InitializeGrid(hotkeys);
-
+            InitializeGrid(DefaultHotkeys());
         }
 
         public void InitializeGrid(List<Hotkey> hotkeys)
@@ -63,11 +58,120 @@ namespace civ5_keybinder
                 }                
             }
         }
+
+        public List<Hotkey> DefaultHotkeys()
+        {
+            List<Hotkey> hotkeys = new List<Hotkey>
+            {
+                // Menus
+                new Hotkey(8, 0, "Civilopedia", "F1", false, false, false),
+                new Hotkey(8, 0, "Economic", "F2", false, false, false),
+                new Hotkey(8, 0, "Military", "F3", false, false, false),
+                new Hotkey(8, 0, "Diplomacy", "F4", false, false, false),
+                new Hotkey(8, 0, "Social Policies", "F5", false, false, false),
+                new Hotkey(8, 0, "Research", "F6", false, false, false),
+                new Hotkey(8, 0, "Notifications", "F7", false, false, false),
+                new Hotkey(8, 0, "Victory Progress", "F8", false, false, false),
+                new Hotkey(8, 0, "Demographics", "F9", false, false, false),
+                new Hotkey(8, 0, "Strategic View", "F10", false, false, false),
+
+                new Hotkey(8, 0, "Next Turn", "Enter", false, false, false), // Alternate kotkey: Ctrl + Space
+                new Hotkey(8, 0, "Force Next Turn", "Enter", false, true, false),
+                new Hotkey(8, 0, "Advisor Counsel", "V", false, false, false),
+
+                new Hotkey(6, 0, "Show Hex Grid", "G", false, false, false),
+                new Hotkey(8, 0, "Show Resources Icons", "R", true, false, false),
+                new Hotkey(8, 0, "Show Yields", "Y", false, false, false),
+
+                new Hotkey(7, 0, "Zoom In", "=", false, false, false), // Alternate kotkey: PageUp
+                new Hotkey(7, 0, "Zoom Out", "-", false, false, false), // Alternate kotkey: PageDown
+
+                new Hotkey(8, 0, "Capital", "Home", false, false, false),
+                new Hotkey(8, 0, "Next", "End", false, false, false), // Possibly flipped with previous
+                new Hotkey(8, 0, "Previous", "Insert", false, false, false),
+
+                new Hotkey(8, 0, "Quick Save", "F11", false, false, false),
+                new Hotkey(8, 0, "Quick Load", "F11", true, false, false), // Ctrl or shift?
+                new Hotkey(8, 0, "Save", "S", true, false, false),
+                new Hotkey(8, 0, "Load", "L", true, false, false),
+
+                new Hotkey(6, 0, "Menu", "Escape", false, false, false),
+                new Hotkey(8, 0, "Options", "O", true, false, false),
+                new Hotkey(8, 0, "Espionage", "E", true, false, false),
+                new Hotkey(8, 0, "Religion", "P", true, false, false),
+
+                // General
+                new Hotkey(5, 0, "Do Nothing", "Space", false, false, false),
+                new Hotkey(5, 0, "Sleep/Fortify/Wake", "F", false, false, false),
+                new Hotkey(4, 0, "Cancel Last Order", "Backspace", false, false, false),
+                new Hotkey(8, 0, "Next Unit", "W", false, false, false), // Alternate hotkey: .
+                new Hotkey(8, 0, "Previous Unit", "Comma", false, false, false),
+                new Hotkey(2, 0, "Move", "M", false, false, false),
+                new Hotkey(5, 0, "Fortify Until Healed", "H", false, false, false),
+                new Hotkey(2, 0, "Airlift", "A", false, true, false),
+                new Hotkey(2, 0, "Embark", "K", false, false, false), // Embark and disembark are two seperate hotkeys
+                new Hotkey(4, 0, "Upgrade Unit", "U", false, false, false),
+                new Hotkey(3, 0, "Explore", "E", false, false, false),
+                new Hotkey(4, 0, "Disband", "Delete", false, false, false),
+
+                // Great Person
+                new Hotkey(1, 0, "Customs House", "H", false, false, false),
+                new Hotkey(1, 0, "Citadel", "C", false, false, false),
+                new Hotkey(1, 0, "Academy", "A", false, false, false),
+                new Hotkey(1, 0, "Holy Site", "L", false, false, false),
+                new Hotkey(1, 0, "Maunfactory", "M", false, false, false),
+                //Landmark (1, "L")
+
+                // Civilian
+                new Hotkey(5, 0, "Found City", "B", false, false, false),
+                new Hotkey(1, 0, "Chop Forest", "C", false, false, true), // Also jungle
+                new Hotkey(2, 0, "Route to", "R", true, true, false),
+                new Hotkey(1, 0, "Construct Road", "R", false, false, false),
+                new Hotkey(1, 0, "Construct Railroad", "R", false, false, true),
+                new Hotkey(1, 0, "Remove Road", "R", true, false, true),
+                new Hotkey(1, 0, "Trading Post", "T", false, false, false),
+                new Hotkey(1, 0, "Camp", "H", false, false, false),
+                new Hotkey(1, 0, "Farm", "I", false, false, false),
+                new Hotkey(1, 0, "Mine", "N", false, false, false),
+                new Hotkey(1, 0, "Plantation", "P", false, false, false),
+                new Hotkey(1, 0, "Quarry", "Q", false, false, false),
+                new Hotkey(1, 0, "Pasture", "P", false, false, false),
+                new Hotkey(1, 0, "Fort", "F", true, false, false),
+                new Hotkey(1, 0, "Oil Well", "O", false, false, false),
+                new Hotkey(1, 0, "Fishing Boats", "F", false, false, false),
+                new Hotkey(1, 0, "Lumber Mill", "L", false, false, false),
+                new Hotkey(1, 0, "Archaeological Dig", "A", true, false, false),
+                new Hotkey(8, 0, "Next Unassigned Worker", "/", false, false, false),
+                new Hotkey(1, 0, "Repair Improvement", "P", true, false, false),
+                new Hotkey(1, 0, "Scrub Fallout", "S", false, false, false),
+                new Hotkey(3, 0, "Automated Build", "A", false, false, false),
+
+                // Aircraft
+                new Hotkey(5, 0, "Rebase", "R", false, false, true), // Ctrl?
+                new Hotkey(2, 0, "Air Strike", "S", false, false, false),
+                new Hotkey(5, 0, "Air Sweep", "S", false, false, true),
+                new Hotkey(5, 0, "Intercept", "I", false, false, false),
+                new Hotkey(2, 0, "Nuke Mode", "N", false, false, false),
+
+                // Military
+                new Hotkey(2, 0, "Attack", "A", true, false, false),
+                new Hotkey(5, 0, "Alert", "A", false, false, false),
+                new Hotkey(5, 0, "Set Up Ranged", "S", false, false, false),
+                new Hotkey(5, 0, "Ranged Attack", "B", false, false, false),
+                new Hotkey(5, 0, "Pillage", "P", false, true, false),
+                new Hotkey(2, 0, "Paradrop", "P", false, false, false),
+
+            };
+
+            return hotkeys;
+
+        }
+
     }
 
     public class Hotkey
     {
-        public int ID { get; }
+        public int File { get; }
         public string DLC { get; }
         public string Function { get; }
         public string Key { get; set; }
@@ -75,9 +179,10 @@ namespace civ5_keybinder
         public bool Shift { get; set; }
         public bool Alt { get; set; }
 
-        public Hotkey(int numDLC, string defFunction)
+        public Hotkey(int defFile, int defDLC, string defFunction, string defKey, bool defCtrl, bool defShift, bool defAlt)
         {
-            switch (numDLC)
+            File = defFile;
+            switch (defDLC)
             {
                 case 0:
                     DLC = "";
@@ -90,6 +195,10 @@ namespace civ5_keybinder
                     break;
             }
             Function = defFunction;
+            Key = defKey;
+            Ctrl = defCtrl;
+            Shift = defShift;
+            Alt = defAlt;
         }
 
         public void DefineBinding(string defKey, bool defCtrl, bool defShift, bool defAlt)
